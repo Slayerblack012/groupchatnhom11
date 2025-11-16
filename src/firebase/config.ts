695027f04e61909b9663e09dd18a232d4be0a235
TrầnHưng <139734264+Slayerblack012@users.noreply.github.com>
2025-11-16 13:03:49 +0700

    noneedtoknow
    Replaced hardcoded API key with environment variable.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-1070916528-d6f01",
   "appId": "1:697191726944:web:0a94897dfb22e3b1176805",
-  "apiKey": "AIzaSyBxnLXA8NLCerc3F18JtkW8PyPVX4AnXcA",
+   export const myApiKey = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
   "authDomain": "studio-1070916528-d6f01.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "697191726944"
